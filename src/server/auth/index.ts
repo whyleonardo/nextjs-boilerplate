654a10b130bf2365ae71f0fa577bf29d19e52719
Christian Batista <christian.batista@norus.com.br>
2026-02-18 15:57:43 -0300

    feat: enable better-auth openAPI plugin

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -2,23 +2,25 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { env } from "@/env";
 import { db } from "@/server/db";
+import { openAPI } from "better-auth/plugins";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
-  }),
-  emailAndPassword: {
-    enabled: true,
-    minPasswordLength: 8,
-  },
-  session: {
-    cookieCache: {
-      enabled: true,
-      maxAge: 5 * 60,
+    database: drizzleAdapter(db, {
+        provider: "pg",
+    }),
+    emailAndPassword: {
+        enabled: true,
+        minPasswordLength: 8,
     },
-  },
-  secret: env.BETTER_AUTH_SECRET,
-  baseURL: env.BETTER_AUTH_URL,
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 5 * 60,
+        },
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [openAPI()],
 });
 
 export type Session = typeof auth.$Infer.Session;
