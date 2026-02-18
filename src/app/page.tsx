6532c58700af0445ad06130dfac5a9b81c6ae47e
Christian Batista <christian.batista@norus.com.br>
2026-02-18 15:57:49 -0300

    chore: reformat page.tsx indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="font-bold text-2xl">
-        <Link href="/sign-in">App</Link>
-      </h1>
-    </div>
-  );
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="font-bold text-2xl">
+                <Link href="/sign-in">App</Link>
+            </h1>
+        </div>
+    );
 }
